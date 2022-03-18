768af2d10a64f70e64d5196ba3cf213b84076bd2
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-03-18 15:00:14 +0530

    something

--- da2/1.cpp
+++ da2/1.cpp
@@ -2,26 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
    int sum = 0;
    int a[100];
-double start; 
-double end; 
-start = omp_get_wtime(); 
+   double start;
+   double end;
+   start = omp_get_wtime();
 
-for(int i = 0; i < 100; i++){
-       a[i] = i + 1;
-   }
-   #pragma omp parallel reduction (+:sum)
+   for (int i = 0; i < 100; i++)
    {
-        #pragma omp for schedule(auto)
-        for(int i = 0; i < 100; i++){
-           sum = sum + a[i];
-        }  
-    }
-    printf("The total sum is %d\n\n\n", sum);
+      a[i] = i + 1;
+   }
+#pragma omp parallel reduction(+ \
+                               : sum)
+   {
+#pragma omp for schedule(auto)
+      for (int i = 0; i < 100; i++)
+      {
+         sum = sum + a[i];
+      }
+   }
+   printf("The total sum is %d\n\n\n", sum);
 
-end = omp_get_wtime(); 
-printf("Work took %f seconds\n", end - start);
+   end = omp_get_wtime();
+   printf("Work took %f seconds\n", end - start);
 }
